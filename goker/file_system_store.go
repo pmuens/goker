e7ac9402ec3765b0a3f7f95e1439e6065d67c83a
Philipp Muens <philipp@muens.io>
2024-09-30 15:00:22 +0200

    Sort players in league by wins

--- goker/file_system_store.go
+++ goker/file_system_store.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
 )
 
 type FileSystemPlayerStore struct {
@@ -30,6 +31,9 @@ func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 }
 
 func (f *FileSystemPlayerStore) GetLeague() League {
+	sort.Slice(f.league, func(i, j int) bool {
+		return f.league[i].Wins > f.league[j].Wins
+	})
 	return f.league
 }
 
